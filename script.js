b6a0961f40fba1f59aaf910b73c96dceeb95e802
Andre.Cabral <andre.cabral@db1.com.br>
2024-03-11 15:58:58 -0300

    finalizando

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 // @ts-nocheck
-// document.getElementById("calculadoraImcInfoInputAltura").addEventListener("input", function (event) {
-//     const input = event.target;
-//     const valor = input.value;
+document.getElementById("calculadoraImcInfoInputAltura").addEventListener("input", function (event) {
+    const input = event.target;
+    const valor = input.value;
 
-//     const valorNumerico = valor.replace(/[^\d.]/g, "");
+    const valorNumerico = valor.replace(/[^\d.]/g, "");
 
-//     const partes = valorNumerico.split(".");
-//     const parteInteira = partes[0];
-//     const parteDecimal = partes.length > 1 ? partes[1] : "";
+    const partes = valorNumerico.split(".");
+    const parteInteira = partes[0];
+    const parteDecimal = partes.length > 1 ? partes[1] : "";
 
-//     let novoValor = parteInteira + "." + parteDecimal;
+    let novoValor = parteInteira + "." + parteDecimal;
 
-//     input.value = novoValor;
-// });
+    input.value = novoValor;
+});
 
 function getNome() {
     const nome = document.getElementById("calculadoraImcInfoInputNome").value;
@@ -42,6 +42,10 @@ function getPeso() {
     return peso;
 }
 
+function getTextoImc() {
+    return `Seu IMC é:`;
+}
+
 function calcularIMC() {
     const nome = getNome();
     document.getElementById("nome").innerHTML = `${nome}`;
@@ -58,22 +62,25 @@ function calcularIMC() {
     const sexo = getSexo();
     document.getElementById("sexo").innerHTML = `${sexo}`;
 
+    const textoImc = getTextoImc();
+    document.getElementById("textoSeuIMC").innerHTML = `${textoImc}`
+
     const imc = (peso / (altura * altura));
 
     let resultado;
 
     if (imc < 18.5) {
-        resultado = `Seu imc é ${imc.toFixed(2)} Baixo Peso`;
+        resultado = `${imc.toFixed(2)} Baixo Peso`;
     } else if (imc < 24.9) {
-        resultado = `Seu imc é ${imc.toFixed(2)} Peso normal`;
+        resultado = `${imc.toFixed(2)} Peso normal`;
     } else if (imc < 29.9) {
-        resultado = `Seu imc é ${imc.toFixed(2)} Sobrepeso`;
+        resultado = `${imc.toFixed(2)} Sobrepeso`;
     } else if (imc < 34.9) {
-        resultado = `Seu imc é ${imc.toFixed(2)} Obesidade Grau 1`;
+        resultado = `${imc.toFixed(2)} Obesidade Grau 1`;
     } else if (imc < 39.9) {
-        resultado = `Seu imc é ${imc.toFixed(2)} Obesidade Grau 2`;
+        resultado = `${imc.toFixed(2)} Obesidade Grau 2`;
     } else {
-        resultado = `Seu imc é ${imc.toFixed(2)} Obesidade Grau 3`
+        resultado = `${imc.toFixed(2)} Obesidade Grau 3`
     }
-    document.getElementById("resultado").innerHTML = resultado;
+    document.getElementById("resultadoIMC").innerHTML = resultado;
 }
